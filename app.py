fef09f4bafe2c07e8a41a0bc505a3ae3de348959
Madame Medusa <138992924+mysteriousbug@users.noreply.github.com>
2024-04-10 13:30:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ st.title('Pronunciation Correction Tool')
 audio_file = st.file_uploader("Upload an audio file", type=['mp3', 'wav'])
 
 if audio_file:
-    st.write("Audio File uploaded successfully!")
+    st.write("Audio file uploaded successfully!")
 
     # Button to trigger pronunciation correction
     if st.button('Correct Pronunciation'):
         # Process audio file
-        corrected_text = "ABBA"
+        corrected_text = "ಒಂದು ಸಾರ್ತಿ ಈ ಪಾರ್ಶ್ವಚಿತ್ರ ನಿರ್ಮಾಣವಾದ ನಂತರ"
         error_percentage = 26.35
-        st.write('Speech To Text:', corrected_text)
+        st.write('Speech-To-Text:', corrected_text)
         st.write('Error in Pronunciation:', error_percentage)
+        st.write('Correct Audio')
         st.audio(audio_file)
