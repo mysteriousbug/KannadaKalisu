f3c61822cdf49b7dab2eace69fdfc19fd9c1c45e
Ananya Aithal <ananya.aithal666@gmail.com>
2025-04-21 15:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from runner import converted_text
 # Set up your Streamlit interface
 st.title('Pronunciation Correction Tool')
-corrected_text = "converted_text
+corrected_text = converted_text
 error_percentage = 0.6851944976699905
 st.write('Speech-To-Text:', corrected_text)
 st.write('Audio Similarity:', error_percentage)
